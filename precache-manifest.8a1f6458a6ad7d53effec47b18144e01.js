94f1d7f2b1228c7a1fd614eee846cbb079524f5f
Nitin Jha <nitin.jha6@gmail.com>
2020-04-27 09:53:43 +0530

    Updates

---
+++ precache-manifest.8a1f6458a6ad7d53effec47b18144e01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "514ebb03c8f26a7de07c9adf349e4a34",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c845a02bfb9ade95c644",
+    "url": "/monster-rolodex/static/css/main.542328ad.chunk.css"
+  },
+  {
+    "revision": "a1cd5040a1be0b917b7e",
+    "url": "/monster-rolodex/static/js/2.a6099e3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.a6099e3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c845a02bfb9ade95c644",
+    "url": "/monster-rolodex/static/js/main.44a849f8.chunk.js"
+  },
+  {
+    "revision": "e4c6a2aa315bdd9de81e",
+    "url": "/monster-rolodex/static/js/runtime-main.403146a8.js"
+  }
+]);
